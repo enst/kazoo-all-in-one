2a2d33ea08e577731315f9455be0fc90b1d76c4d
Bing Li <enst.bupt@gmail.com>
2015-03-14 08:49:48 -0400

    Update first_time.sh

--- first_time.sh
+++ first_time.sh
@@ -2,9 +2,7 @@
 
 source ./setup_common 
 
-yum reinstall -y kazoo-configs
-
-chown kazoo:daemon /opt/kazoo/log -R 
+#yum reinstall -y kazoo-configs
 
 sed -i '/port/s/15984/5984/' /etc/kazoo/config.ini 
 sed -i '/port/s/15986/5986/' /etc/kazoo/config.ini 
